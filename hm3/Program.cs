ff8b382c044511f9eb73e5e0a8d09d3bcc386b2c
Katerina <katerina3113@bk.ru>
2023-02-08 02:10:10 +0300

    Домашнее задание 3

---
+++ hm3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int countOfRows = ReadInt($"Введите колличество строк:");
+int countOfColumns = ReadInt($"Введите колличество столбцов:");
+
+int[,] matrix = new int[countOfRows, countOfColumns];
+InitMatrix(matrix);
+PrintMatrix(matrix);
+
+void InitMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i=0; i < matrix.GetLength(0); i++)
+    {
+        for (int j=0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] average = new double[matrix.GetLength(1)];
+
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        result += matrix[j, i];
+    }
+    average[i] = result / matrix.GetLength(0);
+}
+PrintM(average);
+
+void PrintM (double[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        Console.Write($"{matrix[i]} ");
+    }
+    Console.WriteLine();
+}
